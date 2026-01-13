58387d3b85da8d4b476f644b8ce5b9a27cff35ad
HARIBASKAR-K <haribaskar316@gmail.com>
2026-01-13 20:32:11 +0530

    Initial Commit - api/axios

--- frontend-ecommerce/src/api/axios.js
+++ frontend-ecommerce/src/api/axios.js
@@ -1,15 +1,19 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: import.meta.env.VITE_API_URL || "https://final-ecommerce-m1h5.onrender.com/api",
+  withCredentials: true
 });
 
-API.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
-});
+API.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
 
 export default API;
