2b90d4da0904b0973819ea587b9becdfd4e80ed0
HARIBASKAR-K <haribaskar316@gmail.com>
2026-01-13 21:23:42 +0530

    third  Commit - api/axios

--- frontend-ecommerce/src/api/axios.js
+++ frontend-ecommerce/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://final-ecommerce-m1h5.onrender.com/api",
 });
 
 API.interceptors.request.use((config) => {
