cbc7cee49b53db75cc1d8e16a7373025f41fe8a0
HARIBASKAR-K <haribaskar316@gmail.com>
2026-01-13 22:01:56 +0530

    2nd modified api and server

--- backend-ecommerce/server.js
+++ backend-ecommerce/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cors from "cors";
+
 import productRoutes from "./routes/productRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import orderRoutes from "./routes/orderRoutes.js";
@@ -10,29 +11,34 @@ dotenv.config();
 
 const app = express();
 
+/* ====================== CORS FIX ====================== */
 app.use(cors({
-  origin: "https://final-ecommerce-swart.vercel.app",
-  credentials: true,
-  methods: ["GET","POST","PUT","DELETE","OPTIONS"],
+  origin: [
+    "https://final-ecommerce-two.vercel.app",
+    "http://localhost:5173"
+  ],
+  methods: ["GET","POST","PUT","DELETE"],
   allowedHeaders: ["Content-Type","Authorization"]
 }));
+/* ===================================================== */
 
 app.use(express.json());
 
+/* ====================== ROUTES ====================== */
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/orders", orderRoutes);
+/* =================================================== */
 
 app.get("/", (req, res) => {
   res.send("API Running");
 });
 
-app.get("/ping", (req, res) => {
-  res.send("pong");
-});
-
+/* ====================== DB ====================== */
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected"))
   .catch(err => console.log(err));
+/* =============================================== */
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
